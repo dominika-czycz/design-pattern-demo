3312213ee1ef5d4ec11c3d5a2f92004c456cdd2f
Dominika Czycz <external.dominika.czycz@kuehne-nagel.com>
2024-04-07 17:48:11 +0200

    Singleton small improvements

--- src/test/java/com/patterns/demo/creational/singleton/veryexpensiveclasses/bad/DoubleChackedLockingVolatileSingletonTest.java
+++
@@ -1,7 +0,0 @@
-package com.patterns.demo.creational.singleton.veryexpensiveclasses.bad;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class DoubleChackedLockingVolatileSingletonTest {
-
-}
